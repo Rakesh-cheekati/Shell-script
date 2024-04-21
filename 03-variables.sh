f75b6a417b5038c5d5acb51d822638c2724b5a90
Rakesh-cheekati <rakeshchowdary01.ch@gmail.com>
2024-04-21 20:40:21 +0530

    Adding 3rd script which is for secret variables passing through command line

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Try to pass the pass the variables values as a secrets from command line
+
+echo "The username is ::"
+
+read USERNAME #here USERNAME is a variable
+
+echo "The password is ::"
+
+read PASSWORD #here PASSWORD is a variale
+
+echo "The username is $USERNAME, The password is $PASSWORD"
