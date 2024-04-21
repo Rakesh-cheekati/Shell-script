f721db0f610d9c84433b6fa05b759afb36586520
Rakesh-cheekati <rakeshchowdary01.ch@gmail.com>
2024-04-21 20:44:02 +0530

    modified it to hide the content while typing

--- 03-variables.sh
+++ 03-variables.sh
@@ -10,3 +10,17 @@ echo "The password is ::"
 read PASSWORD #here PASSWORD is a variale
 
 echo "The username is $USERNAME, The password is $PASSWORD"
+
+
+#the above script while we type it will be visible if we want to hide it we can use "-s"
+
+
+echo "The username is ::"
+
+read -s USERNAMEHIDDEN #here USERNAME is a variable
+
+echo "The password is ::"
+
+read -s PASSWORDHIDDEN #here PASSWORD is a variale
+
+echo "The username is $USERNAMEHIDDEN, The password is $PASSWORDHIDDEN"
